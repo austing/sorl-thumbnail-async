fffa4f68f1f0e8be8d4e20deb72b896b66d8c8b4
Joeri Djojosoeparto <joeri@breadandpepper.com>
2014-09-22 12:19:49 +0200

    Overwrite default kvsstore with thumbnail when none

--- thumbnail/backend.py
+++ thumbnail/backend.py
@@ -16,17 +16,33 @@ class AsyncThumbnailBackend(ThumbnailBackend):
         # very costly for remote storages.
         # Furthermore, I have added following code to reduce/prevent duplicate
         # tasks in celery. It's hacky.
-        try:
-            default.kvstore.get_or_set(source)
-            default.kvstore.set(thumbnail, source)
-            return thumbnail
-        except IOError:
-            pass
+        
         # Finally, if there is no thumbnail, we create one.
         from .tasks import create_thumbnail
-        create_thumbnail.delay(source.name, geometry_string, **options)
+        job = create_thumbnail.delay(source.name, geometry_string, **options)
         # Sometimes thumbnail generation takes quite some time, just return
         # the original image then.
+        if job:
+            geometry = parse_geometry(geometry_string)
+            # We can't add a source row to the kvstore without the size
+            # information being looked up, so add dummy information here
+            # We'll need to correct this information when we generate the thumbnail
+            source.set_size(geometry)
+            default.kvstore.get_or_set(source)
+
+            # We don't want to do any file access in this thread, so we tell sorlery
+            # to proceed as normal and cheekily update the name and storage after
+            # the hash has been calculated.
+            thumbnail.set_size(geometry)
+            default.kvstore.set(thumbnail, source)
+
+            # Now we go back and manually update the thumbnail to point at the source image
+            # Hopefully someone can suggest a better way to do this ... but the sorl internals
+            # don't make it easy to.
+            rawvalue = default.kvstore._get_raw(add_prefix(thumbnail.key))
+            rawvaluearr = deserialize(rawvalue)
+            rawvaluearr['name'] = file_.name
+            default.kvstore._set_raw(add_prefix(thumbnail.key), serialize(rawvaluearr))
         
         thumbnail.name = file_.name
         return thumbnail
